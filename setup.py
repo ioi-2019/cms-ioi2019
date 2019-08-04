7712f10388abb501cced29b54a8efce6fbc30919
William Di Luigi <williamdiluigi@gmail.com>
2019-08-04 19:06:38 +0400

    re-enable pascal

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ setup(
             "Haskell / ghc=cms.grading.languages.haskell_ghc:HaskellGhc",
             "Java 1.4 / gcj=cms.grading.languages.java14_gcj:Java14Gcj",
             "Java / JDK=cms.grading.languages.java_jdk:JavaJDK",
-            #"Pascal / fpc=cms.grading.languages.pascal_fpc:PascalFpc",
+            "Pascal / fpc=cms.grading.languages.pascal_fpc:PascalFpc",
             "PHP=cms.grading.languages.php:Php",
             "Python 2 / CPython=cms.grading.languages.python2_cpython:Python2CPython",
             "Python 3 / CPython=cms.grading.languages.python3_cpython:Python3CPython",
